624ffc7210e30831bc5cb798e3db66cf0b224557
irene-gomez <irene@irenegf.com>
2019-06-14 15:54:03 +0200

    Add loading

---
+++ src/components/Loading/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles.css';
+import pokeball from '../../images/pokeball.svg';
+
+const Loading = () => {
+    return <div className="pokeball-loading" />;
+};
+
+export default Loading;
